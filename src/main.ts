71a355adba5aaf4f2fbfcb5ea186cc2ff2ea00e9
lauechev <laura.echeverri3@gmail.com>
2025-02-07 16:31:51 -0500

    🧝‍♀️add typesize input

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,39 @@
 import './scss/styles.scss';
+document.addEventListener('DOMContentLoaded', () => {
+  const sentenceTag = document.querySelector<HTMLInputElement>(`input[type="text"]`);
+  const typesizeTag = document.querySelector<HTMLInputElement>(`input[name="typesize"]`);
+  const typesizeLabel = document.querySelector<HTMLSpanElement>('.typesize-label');
+  const outputTag = document.querySelector<HTMLTextAreaElement>('.output');
+  const originalText = outputTag?.value || '';
 
-const sentenceTag = document.querySelector<HTMLInputElement>(`input[type="text"]`);
-const outputTag = document.querySelector<HTMLDivElement>('.output');
+  //when i type in my sentence tag, update the output tag accordingly
+  //if there is no value, put in the original text
+  console.log(outputTag);
 
-//when i type in my sentence tag, update the output tag accordingly
+  if (sentenceTag && outputTag) {
+    sentenceTag.addEventListener('keyup', function () {
+      if (this.value) {
+        outputTag.value = this.value;
+      } else {
+        outputTag.value = originalText;
+      }
+    });
+  }
 
-if (sentenceTag && outputTag) {
-  sentenceTag.addEventListener('keyup', function () {
-    if (this.value) {
-      outputTag.innerHTML = this.value;
-    } else {
-      outputTag.innerHTML = 'The quick brown fox...';
+  //when i type in my output tag, update the sentence tag accordingly
+  if (sentenceTag && outputTag) {
+    outputTag?.addEventListener('keyup', function () {
+      sentenceTag.value = this.value;
+    });
+  }
+
+  //when i change my typesize slider, update the text next to it and change the outputTag's font size
+  typesizeTag?.addEventListener('input', function () {
+    if (outputTag) {
+      outputTag.style.fontSize = this.value + 'px';
+      if (typesizeLabel) {
+        typesizeLabel.innerHTML = this.value + 'px';
+      }
     }
-    // outputTag.innerHTML = this.value;
   });
-}
+});
